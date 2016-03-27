c2913b3de9ab3c5899390b66a2d176a10c487153
Pradeep Ashok <pradeepkumar.ashok@gmail.com>
2016-03-27 18:57:10 -0500

    Pump Output Calculations Revised by Deep Joshi

--- computeTotalPumpOutputDuplexApproach1.m
+++ computeTotalPumpOutputDuplexApproach1.m
@@ -18,7 +18,7 @@
    %}
    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
    
-   function totalPumpOutput_gpm  = computeTotalPumpOutputApproach1(linerSize_in, strokeLength_in, efficiency_percent) 
+   function totalPumpOutput_gpm  = computeTotalPumpOutputDuplexApproach1(linerSize_in, strokeLength_in, efficiency_percent, rodDiameter_in) 
      
    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
    %{
@@ -32,6 +32,7 @@
    1. Efficiency
    2. Linersize
    3. Strokelength
+   4. Rod Diameter
       
    Other Comments:
      
@@ -45,18 +46,26 @@
     linerSize_in = 6;
     strokeLength_in = 12;
     efficiency_percent = 90;
+    rodDiameter_in = 2;
     end
         
     if nargin < 2
     strokeLength_in = 12;
     efficiency_percent = 90;
+    rodDiameter_in = 2;
     end
     
     if nargin < 3
     efficiency_percent = 90;
+    rodDiameter_in = 2;
     end
     
- 
+  if nargin < 4
+    rodDiameter_in = 2;
+     end
+    
+     
+  pumpType = 'SingleActing';   % SingleActing/DoubleActing
      % Load the dataset into memory
      
      h = waitbar(0, 'Loading Data'); 
@@ -65,7 +74,7 @@
      % Separate arrays are created for each data channel for ease of use. 
      % Not all of the channels will be needed for all derived data calculations. 
      
-     time_sec = rawDataAllChannels(:,1)
+     time_sec = rawDataAllChannels(:,1);
      blockHeight_feet = rawDataAllChannels(:,2);
      flowOut_percent = rawDataAllChannels(:,3);
      hookLoad_klbf = rawDataAllChannels(:,4);
@@ -80,11 +89,31 @@
      
      waitbar(0.3, h, 'Performing Calculations'); 
          
-     for i=1:length(dataInstances_nounit)
+     if (pumpType == 'SingleActing')
+         
+         disp(pumpType)
+         
+         for i=1:length(blockHeight_feet)
         
          totalStrokesPerMinute_spm(i) = strokesPerMinute1_spm(i)+strokesPerMinute2_spm(i);
-         totalPumpOutput_gpm(i) = 0.000102*(linerSize_in.*linerSize_in)*strokeLength_in*efficiency_percent*totalStrokesPerMinute_spm(i);
+         totalPumpOutput_gpm(i) = 0.00006804*(linerSize_in.*linerSize_in)*strokeLength_in*efficiency_percent*totalStrokesPerMinute_spm(i);
      
+         end
+          
+     elseif (pumpType == 'DoubleActing')
+         
+         disp(pumpType)
+           
+         for i=1:length(blockHeight_feet)
+        
+         totalStrokesPerMinute_spm(i) = strokesPerMinute1_spm(i)+strokesPerMinute2_spm(i);
+         totalPumpOutput_gpm(i) = 0.00006804*((2*(linerSize_in.*linerSize_in))-(rodDiameter_in.*rodDiameter_in))*strokeLength_in*efficiency_percent*totalStrokesPerMinute_spm(i);
+         
+         end
+     else
+         
+         disp('Pump Type Should be SingleActing or DoubleActing')
+         
      end
      
      totalPumpOutput_gpm = totalPumpOutput_gpm';
@@ -99,7 +128,7 @@
              
      outfileName = fullfile(pwd, 'OutputResults', 'AnalysisResults.csv' );
      dataToExport = [rawDataAllChannels,totalPumpOutput_gpm]; 
-     header='Block Height(feet),Flow Out(%),Hookload(klbf),Top Drive Speed(RPM),Strokes Per Minute #1,Strokes Per Minute #2,Standpipe Pressure (psi),Top Drive Torque (ftlb),Total Pump Output(gpm)'; 
+     header='Time(Sec),Block Height(feet),Flow Out(%),Hookload(klbf),Top Drive Speed(RPM),Strokes Per Minute #1,Strokes Per Minute #2,Standpipe Pressure (psi),Top Drive Torque (ftlb),Data Instance, Total Pump Output(gpm)'; 
      dlmwrite(outfileName,header,'delimiter','');
      dlmwrite(outfileName,dataToExport,'delimiter',',','-append');
      
